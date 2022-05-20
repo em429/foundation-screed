ab68dc0c49b5be124e2bb000230a1709dff00f23
qirpi <mate@lambd.ax>
2022-05-20 17:56:38 +0200

    add env example

---
+++ src/lib/env_example.js
@@ -0,0 +1,25 @@
+export const ENV = {
+    POOL_NAME: 'LottoNonce',
+    SUPPORT_EMAIL: "support@lottononce.com",
+    POOL_URL: 'https://lottononce.com',
+    STRATUM_URL: 'lottononce.com',
+    FOUNDATION_API_BASE: 'https://api.example.com/api/v1',
+
+
+    BLOCK_REWARD: 6.25,  // BTC current block reward
+    BLOCK_TIME_IN_SECONDS: 600, // 10 minutes for BTC
+    BLOCK_TIME_UNIT: 'minutes',
+    BLOCK_TIME_UNIT_DIVIDER: 60,// to make seconds minutes in template
+
+    BLOCK_EXPLORER_URL: 'https://',
+
+    // Convert Hash to Petahash by dividing it by 1 trillion -->
+    HASHRATE_DISPLAY_MULTIPLIER: 1000 ** 5,
+    HASHRATE_DISPLAY_UNIT: 'PH/s',
+
+    WORKER_HASHRATE_DISPLAY_UNIT: 'MH/s',
+    WORKER_HASHRATE_DISPLAY_MULTIPLIER: 1000 ** 2,
+
+    // API limiter's error string, leave as is
+    LIMITER_ERROR_TEXT: 'Unexpected token T in JSON at position 0',
+}
